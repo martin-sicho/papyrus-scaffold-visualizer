259b6da604929faab5c6d83ca6099228cf6af45f
martin-sicho <sicho.martin@gmail.com>
2022-10-04 17:34:11 +0200

    refactored and cleaned the papyrus package

---
+++ papyrus/__init__.py
@@ -0,0 +1,9 @@
+"""
+__init__
+
+Created by: Martin Sicho
+On: 04.10.22, 16:19
+"""
+import os
+
+OUTDIR = os.path.join(os.path.dirname(__file__), 'data')
--- papyrus/papyrus_filter.py
+++ papyrus/papyrus_filter.py
@@ -1,27 +1,27 @@
-import pandas as pd
-import papyrus_scripts
-from papyrus_scripts.reader import read_papyrus, read_protein_set 
-from papyrus_scripts.preprocess import (keep_quality, keep_source, keep_type,
-                                        keep_organism, keep_accession, keep_protein_class,
-                                        keep_match, keep_contains
-                                       )
+import os.path
+
+from papyrus_scripts.reader import read_papyrus
+from papyrus_scripts.preprocess import keep_quality, keep_accession
 from papyrus_scripts.preprocess import consume_chunks
-import math
+
+from papyrus import OUTDIR
 
 
+def papyrus_filter(acc_key: list, quality: str, drop_duplicates: bool = True, chunk_size : int = 1e5, outfile: str = None):
+    """
+    Filters the downloaded papyrus dataset for quality and accession key (UniProt) and outputs a .tsv file of all compounds fulfilling these requirements.
 
-def papyrus_filter(acc_key: list, chunksize: int, quality: str, name: str):
-    '''
-    This script should filter the downloaded papyrus dataset for quality and accession key (UniProt) and output a .tsv file of all compounds fullfilling these requirements.
-    args:
+    Args:
+        outfile: path to the output file
         acc_key: list of UniProt accession keys
-        chunksize: integer of chunks to process one at the time
-        quality: str with quality of dataset
-        name: string to name the file to save
-    Output: .tsv file
-    '''
+        quality: str with minimum quality of dataset to keep
+        drop_duplicates: boolean to drop duplicates from the final dataset
+        chunk_size: integer of chunks to process one at the time
+    Output:
+        .tsv file with all compounds fulfilling the requirements
+    """
     # read data
-    sample_data = read_papyrus(is3d=False, chunksize=1000_000, source_path="/zfsdata/data/yorick/DrugEx_Papyrus/")
+    sample_data = read_papyrus(is3d=False, chunksize=chunk_size, source_path=OUTDIR)
     print("Read all data.")
 
     # data filters
@@ -30,24 +30,23 @@ def papyrus_filter(acc_key: list, chunksize: int, quality: str, name: str):
     print("Initialized filters.")
 
     # filter data per chunk
-    tot_chunks = math.ceil(60_000_000 / chunksize) # amount of chunks
-    print(f"Starting to filter data for {tot_chunks} chunks of size {chunksize}.")
-    filtered_data = consume_chunks(generator=filter2, progress=True, total=tot_chunks)
+    filtered_data = consume_chunks(generator=filter2)
     print(f"Number of compounds:{filtered_data.shape[0]}")
 
     # filter out duplicate InChiKeys
-    print("Filtering out duplicate molecules")
-    amnt_mols_i = len(filtered_data["InChIKey"])
-    filtered_data.drop_duplicates(subset=["InChIKey"], inplace=True, ignore_index=True)
-    amnt_mols_f = len(filtered_data["InChIKey"])
-    print(f"Filtered out {amnt_mols_i - amnt_mols_f} duplicate molecules")
+    if drop_duplicates:
+        print("Filtering out duplicate molecules")
+        amnt_mols_i = len(filtered_data["InChIKey"])
+        filtered_data.drop_duplicates(subset=["InChIKey"], inplace=True, ignore_index=True)
+        amnt_mols_f = len(filtered_data["InChIKey"])
+        print(f"Filtered out {amnt_mols_i - amnt_mols_f} duplicate molecules")
 
     # write filtered data to .tsv file
-    filtered_data.to_csv(f"/zfsdata/data/yorick/Internship/Files/{name}.tsv", sep= "\t", index=False)
-    print(f"Wrote data to file {name}.tsv")
+    if outfile:
+        filtered_data.to_csv(outfile, sep= "\t", index=False)
+        print(f"Wrote data to file {name}.tsv")
 
-
-
+    return filtered_data
 
 if __name__ == "__main__":
  
@@ -55,12 +54,11 @@ if __name__ == "__main__":
 
     # acc_key = ["P32246", "P51681", "P51685",] # CCR1,5,8 
     acc_key = ["P41597"] # CCR2
-    chunksize = 1000_000
     quality = "low" # choose from {"high", "medium", "low"}
     name = "hCCR2_LIGANDS"
 
     # call function
-    papyrus_filter(acc_key, chunksize, quality, name)
+    papyrus_filter(acc_key, quality, drop_duplicates=False, chunk_size=1000_000, outfile=os.path.join(OUTDIR, f"./{name}.tsv"))
 
 
 
